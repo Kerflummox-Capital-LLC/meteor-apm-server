4dcd6e2a3a94cf447c610421f5e4add352fea849
Don Stephan <donald.m.stephan@gmail.com>
2019-05-30 14:38:56 -0400

    updated engine ntp endpoints

--- server/engine/lib/middlewares/simplentp.js
+++ server/engine/lib/middlewares/simplentp.js
@@ -1,15 +1,15 @@
 var engineUtils = require('../utils');
 
-module.exports = function() {
-  return function(req, res, next) {
+module.exports = function () {
+  return function (req, res, next) {
     var url = req._parsedUrl;
-    if (req.url == '/simplentp/sync') {
+    if (req.url == '/engine/simplentp/sync') {
       // same route used for both OPTIONS and data request
       var headers = {
         'Content-Type': 'text/plain'
       };
       engineUtils.replyWithCors(req, res, 200, new Date().getTime().toString(), headers);
-    } else if (url.pathname == '/simplentp/sync/jsonp') {
+    } else if (url.pathname == '/engine/simplentp/sync/jsonp') {
       var callback = req.query.callback || 'callback';
       res.writeHead(200, { 'Content-Type': 'text/javascript' });
       res.end(callback + '(' + Date.now().toString() + ')');
